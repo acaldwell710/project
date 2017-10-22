fb6f2e9423c528783d24d9d9f027e333e9edd4f1
Amber Caldwell <acaldwell710@gmail.com>
2017-10-22 14:10:28 -0400

    DRY clean up

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
-  let(:wiki) { Wiki.create!(title: "New Wiki Title", body: "New Wiki Body") }
+  let(:wiki) { Wiki.create!(title: "New Wiki Title", body: "New Wiki Wiki Body Bodies") }
+
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
   describe "attributes" do
     it "has title and body attributes" do
-      expect(wiki).to have_attributes(title: "New Wiki Title", body: "New Wiki Body")
+      expect(wiki).to have_attributes(title: "New Wiki Title", body: "New Wiki Wiki Body Bodies")
     end
   end
 end
