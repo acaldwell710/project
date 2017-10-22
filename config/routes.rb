afe13d528a097da2f641d30bcbc05d43cf51d43a
Amber Caldwell <acaldwell710@gmail.com>
2017-10-22 19:16:59 -0400

    added sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :wikis
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
 
