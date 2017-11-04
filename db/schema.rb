aa3fde7ede56aaf808fbec8119e52dfe87148d6a
Amber Caldwell <acaldwell710@gmail.com>
2017-11-04 17:24:57 -0400

    updated collaborate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101210310) do
+ActiveRecord::Schema.define(version: 20171104211752) do
+
+  create_table "collaborators", force: :cascade do |t|
+    t.integer "wiki_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["wiki_id"], name: "index_collaborators_on_wiki_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
